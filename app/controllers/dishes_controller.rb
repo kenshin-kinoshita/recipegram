d3e8913484837b7d77d7904dde45fe94f02cfffd
kenshin.kinoshita <temiyage6240@gmail.com>
2020-12-05 01:46:45 +0000

    料理の更新機能作成

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,5 +1,6 @@
 class DishesController < ApplicationController
   before_action :logged_in_user
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @dish = Dish.find(params[:id])
@@ -18,11 +19,32 @@ class DishesController < ApplicationController
       render 'dishes/new'
     end
   end
+  
+  def edit
+    @dish = Dish.find(params[:id])
+  end
+
+  def update
+    @dish = Dish.find(params[:id])
+    if @dish.update_attributes(dish_params)
+      flash[:success] = "料理情報が更新されました！"
+      redirect_to @dish
+    else
+      render 'edit'
+    end
+  end
+
 
   private
 
     def dish_params
       params.require(:dish).permit(:name, :discription, :portion, :tips,
                                    :reference, :required_time, :popularity, :cook_memo)
+    end
+    
+    def correct_user
+      # 現在のユーザーが更新対象の料理を保有しているかどうか確認
+      @dish = current_user.dishes.find_by(id: params[:id])
+      redirect_to root_url if @dish.nil?
     end
 end
