b5aa44441988e467c4980ff167a4a3a84f7b11fd
kenshin.kinoshita <temiyage6240@gmail.com>
2021-01-07 11:28:24 +0000

    フォロー機能実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,11 @@ end
                popularity: 5,
                cook_memo: "初めて作った割にはうまくできた！")
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
